f20dba17386ef8639fbed0b02e00794ac8d9a799
pinchen652 <hardymike6308@gmail.com>
2021-09-18 08:28:43 +0800

    TestData Problem

--- HW1/TEST.cpp
+++ HW1/TEST.cpp
@@ -7,12 +7,12 @@ using namespace std;
 void solve(tTestData* test_data){
 	fstream ans;
 	ans.open("output.txt",ios::out);
-	int table[20001] = {};
+	int table[5001] = {};
 	for(int i = 0; i < test_data->cnt; i++){
 		for(int j = 0; j < test_data->seq_size[i]; j++){
 			table[test_data->data[i][j]]++;
 		}
-		for(int j = 1; j < 20001; j++){
+		for(int j = 1; j < 5001; j++){
 			while(table[j] > 0){
 				ans << j << " ";
 				table[j]--;
