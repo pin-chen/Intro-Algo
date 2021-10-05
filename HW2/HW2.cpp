da3e6d97bdaa58969f147b28d316eefe22fae2a7
pinchen652 <hardymike6308@gmail.com>
2021-10-05 23:06:17 +0800

    HW2_cpp_optmization

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -1,23 +1,76 @@
+#pragma GCC optimization ("unroll-loops")
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <map>
 #include <fstream>
 using namespace std;
+void intToS(string& OUT, int x){
+	if(x == 0) {
+		OUT.push_back('0');
+		return;
+	}
+	if(x < 0){
+		OUT.push_back('-');
+		x = -x;
+	}
+	int begin = OUT.size();
+	while(x > 0){
+		OUT.push_back('0' + (x % 10) );
+		x /= 10;
+	}
+	int end = OUT.size() - 1;
+	for(int i = begin; i <= begin + (end - begin) / 2; i++){
+		char temp = OUT[i];
+		OUT[i] = OUT[end - i + begin];
+		OUT[end - i + begin] = temp;
+	}
+}
 int main(){
 	ifstream input("input.txt",ios::in);
 	ofstream output("output.txt",ios::out);
 	map< pair<int,int>, int > H[3];
-	string x, s;
+	string s;
 	pair<int, int> point, S, E;
-	stringstream ss;
+	bool negtive, start;
+	int count, x;
 	while(getline(input, s)){
 		switch(s[1]){
 			case 's':{//"Assign"
-				ss << s;
-				ss >> x >> x >> point.first >> point.second;
-				ss >> H[x[0] - 'a'][point];
-				ss.clear();
+				negtive = false;
+				start = true;
+				count = 0;
+				for(int i = 10; i < s.size(); i++){
+					if(start){
+						if(s[i] < '0' || s[i] > '9' ){
+							continue;
+						}
+					}	
+					start = false;
+					if(s[i] == '-'){
+						i++;
+						negtive = true;
+					}
+					x = 0;
+					while(s[i] >= '0' && s[i] <= '9' && i < s.size()){
+						x = x * 10 + (s[i] - '0');
+						i++;
+					}
+					switch(count){
+						case 0:{
+							point.first = x;
+							break;
+						}
+						case 1:{
+							point.second = x;
+							break;
+						}
+						case 2:{
+							H[s[7] - 'a'][point] = negtive ? -x : x;
+							break;
+						}
+					}
+					count++;
+				}
 			}break;
 			case 'e'://"Delete"
 			case 'n'://"Init"
@@ -25,10 +78,19 @@ int main(){
 				break;
 			}
 			case 'r':{//"Print"
+				string OUT;
 				for(auto out: H[2]){
-					output <<  "(" << out.first.first << " " << out.first.second << " " << out.second << ") ";
+					OUT.push_back('(');  
+					intToS(OUT, out.first.first);
+					OUT.push_back(' ');
+					intToS(OUT, out.first.second);
+					OUT.push_back(' ');
+					intToS(OUT, out.second);
+					OUT.push_back(')');
+					OUT.push_back(' ');
 				}
-				output << '\n';
+				OUT.push_back('\n');
+				output << OUT;
 				H[0].clear();
 				H[1].clear();
 				H[2].clear();
