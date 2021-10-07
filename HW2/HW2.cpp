dffe64aaccfdd85689b7ee42ef58abc25d753269
pinchen652 <hardymike6308@gmail.com>
2021-10-07 16:00:49 +0800

    HW2_cpp_optmization2

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimization ("Ofast")
 #pragma GCC optimization ("unroll-loops")
 #include <iostream>
 #include <string>
@@ -30,22 +31,16 @@ int main(){
 	ofstream output("output.txt",ios::out);
 	map< pair<int,int>, int > H[3];
 	string s;
-	pair<int, int> point, S, E;
-	bool negtive, start;
-	int count, x;
+	pair<int, int> S, E;
+	bool negtive;
+	int count, x, i;
 	while(getline(input, s)){
 		switch(s[1]){
 			case 's':{//"Assign"
 				negtive = false;
-				start = true;
 				count = 0;
-				for(int i = 10; i < s.size(); i++){
-					if(start){
-						if(s[i] < '0' || s[i] > '9' ){
-							continue;
-						}
-					}	
-					start = false;
+				for(i = 10; s[i] < '0' || s[i] > '9'; i++);
+				for(; i < s.size(); i++){
 					if(s[i] == '-'){
 						i++;
 						negtive = true;
@@ -57,15 +52,15 @@ int main(){
 					}
 					switch(count){
 						case 0:{
-							point.first = x;
+							S.first = x;
 							break;
 						}
 						case 1:{
-							point.second = x;
+							S.second = x;
 							break;
 						}
 						case 2:{
-							H[s[7] - 'a'][point] = negtive ? -x : x;
+							H[s[7] - 'a'][S] = negtive ? -x : x;
 							break;
 						}
 					}
@@ -78,19 +73,19 @@ int main(){
 				break;
 			}
 			case 'r':{//"Print"
-				string OUT;
+				s.clear();
 				for(auto out: H[2]){
-					OUT.push_back('(');  
-					intToS(OUT, out.first.first);
-					OUT.push_back(' ');
-					intToS(OUT, out.first.second);
-					OUT.push_back(' ');
-					intToS(OUT, out.second);
-					OUT.push_back(')');
-					OUT.push_back(' ');
+					s.push_back('(');  
+					intToS(s, out.first.first);
+					s.push_back(' ');
+					intToS(s, out.first.second);
+					s.push_back(' ');
+					intToS(s, out.second);
+					s.push_back(')');
+					s.push_back(' ');
 				}
-				OUT.push_back('\n');
-				output << OUT;
+				s.push_back('\n');
+				output << s;
 				H[0].clear();
 				H[1].clear();
 				H[2].clear();
@@ -102,22 +97,19 @@ int main(){
 					H[1][S];
 					H[1][E];
 					for(auto mB = H[1].find(S); mB != H[1].find(E); mB++){
-						point = make_pair(mA.first.first, mB->first.second);
 						if(H[0][mA.first] == 0 || H[1][mB->first] == 0) continue;
-						H[2][point] += H[0][mA.first] * H[1][mB->first];
-						if( H[2][point] == 0) H[2].erase( H[2].find(point) );
+						H[2][make_pair(mA.first.first, mB->first.second)] += H[0][mA.first] * H[1][mB->first];
 					}
 				}
 			}break;	
 			case 'd':{//"Add"
-				int temp;
 				for(auto m: H[0]){
-					temp = H[0][m.first] + H[1][m.first];
-					if(temp) H[2][m.first] = temp;
+					x = H[0][m.first] + H[1][m.first];
+					if(x) H[2][m.first] = x;
 				}
 				for(auto m: H[1]){
-					temp = H[0][m.first] + H[1][m.first];
-					if(temp) H[2][m.first] = temp;
+					x = H[0][m.first] + H[1][m.first];
+					if(x) H[2][m.first] = x;
 				}
 			}break;
 		}
