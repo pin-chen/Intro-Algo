1a48605620fb4b348e6458f90f6c0d825902a817
Pin-Shao Chen <hardymike6308@gmail.com>
2021-10-21 22:39:38 +0800

    HW3_v3

--- HW3/HW3.cpp
+++ HW3/HW3.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
 #include <fstream>
-#include <unordered_set>
 using namespace std;
 
-int ans = 0;
+int ans = 0, n, m;
 int R[24] = {};
-//int C[24] = {};
+int C[24] = {};
 
-void solve(int x, int s, int n, bool *chessD1, bool *chessD2, unordered_set<int> C, int e){
-	if(x == s){
+void solve(int x, bool *chessC, bool *chessD1, bool *chessD2){
+	if(x == n - m){
 		ans++;
 	    return;
 	}
-	if(e != -1)C.erase(e); 
-	//if(chessR[R[x]]){
-	//	solve(x + 1, s, n, chessC, chessR, chessD1, chessD2);
-	//	return;
-	//}
-	for(auto y: C){
-		bool temp = chessD1[R[x] + y] || chessD2[n - y + R[x] - 1];
+	for(int y = 0; y < n - m; y++){
+		bool temp =  chessC[C[y]] || chessD1[R[x] + C[y]] || chessD2[n - C[y] + R[x] - 1];
 	    if(!temp){
-	        chessD1[R[x]+y] = true;
-	        chessD2[n-y+R[x]-1] = true;
-	        //chessR[R[x]] = true;
-	        //chessC[y] = true;
-	        solve(x + 1, s, n, chessD1, chessD2, C, y);
-			chessD1[R[x]+y] = false;
-	        chessD2[n-y+R[x]-1] = false;
-	        //chessR[R[x]] = false;
-	        //chessC[y] = false;
+	        chessD1[R[x]+C[y]] = true;
+	        chessD2[n-C[y]+R[x]-1] = true;
+	        chessC[C[y]] = true;
+	        solve(x + 1, chessC, chessD1, chessD2);
+			chessD1[R[x]+C[y]] = false;
+	        chessD2[n-C[y]+R[x]-1] = false;
+	        chessC[C[y]] = false;
 	    }
 	}
 }
@@ -36,7 +28,7 @@ void solve(int x, int s, int n, bool *chessD1, bool *chessD2, unordered_set<int>
 int main(){
 	ifstream in("input.txt", ios::in);
 	ofstream out("output.txt", ios::out);
-	int t, n, m;
+	int t;
 	in >> t;
 	for(int k = 0; k < t; k++){
 		in >> n >> m;
@@ -55,18 +47,18 @@ int main(){
 		for(int i = 0, x = 0; i < n; i++){
 			if(!chessR[i]) R[x++] = i;
 		}
-		unordered_set<int> C;
 		for(int i = 0, x = 0; i < n; i++){
-			if(!chessC[i]) C.insert(i);
+			if(!chessC[i]) C[x++] = i;
 		}
 		ans = 0;
-		if(n - m > 20) {
-			out << ans << '\n';
-			continue;
-		}
-		
-		solve(0, n - m, n, chessD1, chessD2, C, -1);
+		//if(n - m > 20) {
+		//	out << ans << '\n';
+		//	continue;
+		//}
+		solve(0, chessC, chessD1, chessD2);
 		out << ans << '\n';
 	}	
 	return 0;
-}
+} 
+
+//n=20 m=11
