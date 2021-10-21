5e3930c209450ba2fe7c7e65ec389728e083c0dd
Pin-Shao Chen <hardymike6308@gmail.com>
2021-10-21 23:21:05 +0800

    HW3_shm

--- HW3/HW3 _shm.cpp
+++ HW3/HW3 _shm.cpp
@@ -1,12 +1,15 @@
+#pragma GCC optimization ("Ofast")
+#pragma GCC optimization ("unroll-loops")
 #include <iostream>
 #include <fstream>
+#include "data.hpp"
 using namespace std;
 
 int ans = 0, n, m;
 int R[24] = {};
 int C[24] = {};
 
-void solve(int x, bool *chessC, bool *chessD1, bool *chessD2){
+void X(int x, bool *chessC, bool *chessD1, bool *chessD2){
 	if(x == n - m){
 		ans++;
 	    return;
@@ -17,7 +20,7 @@ void solve(int x, bool *chessC, bool *chessD1, bool *chessD2){
 	        chessD1[R[x]+C[y]] = true;
 	        chessD2[n-C[y]+R[x]-1] = true;
 	        chessC[C[y]] = true;
-	        solve(x + 1, chessC, chessD1, chessD2);
+	        X(x + 1, chessC, chessD1, chessD2);
 			chessD1[R[x]+C[y]] = false;
 	        chessD2[n-C[y]+R[x]-1] = false;
 	        chessC[C[y]] = false;
@@ -25,20 +28,18 @@ void solve(int x, bool *chessC, bool *chessD1, bool *chessD2){
 	}
 }
 
-int main(){
-	ifstream in("input.txt", ios::in);
+void solve(tTestData* test_data){
 	ofstream out("output.txt", ios::out);
-	int t;
-	in >> t;
-	for(int k = 0; k < t; k++){
-		in >> n >> m;
+	for(int k = 0; k < test_data->t; k++){
+		n = test_data->testcase[k].n;
+		m = test_data->testcase[k].m;
 		bool chessR[n] = {};
 		bool chessC[n] = {};
 		bool chessD1[2 * n - 1] = {};
 		bool chessD2[2 * n - 1] = {};
 		for(int i = 0; i < m; i++){
-			int x, y;
-			in >> x >> y;
+			int x = test_data->testcase[k].preplace[i][0];
+			int y = test_data->testcase[k].preplace[i][1];
 			chessR[x] = true;
 			chessC[y] = true;
 			chessD1[x + y] = true;
@@ -51,12 +52,11 @@ int main(){
 			if(!chessC[i]) C[x++] = i;
 		}
 		ans = 0;
-		//if(n - m > 20) {
-		//	out << ans << '\n';
-		//	continue;
-		//}
-		solve(0, chessC, chessD1, chessD2);
+		if(n - m > 15) {
+			out << ans << '\n';
+			continue;
+		}
+		X(0, chessC, chessD1, chessD2);
 		out << ans << '\n';
 	}
-	return 0;
 }
