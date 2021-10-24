3f1b6d6be89ca6e898e0a34f0a64b781b7533718
Pin-Shao Chen <hardymike6308@gmail.com>
2021-10-24 14:34:25 +0800

    HW3_list_shm

--- HW3/HW3_list_shm.cpp
+++ HW3/HW3_list_shm.cpp
@@ -2,8 +2,9 @@
 #pragma GCC optimization ("unroll-loops")
 #include <iostream>
 #include <fstream>
+#include "data.hpp"
 using namespace std;
-int n, m, i, t, x, y;
+int n, m, i, k, x, y;
 struct list{
 	list* next;
 	list* prev;
@@ -16,7 +17,7 @@ bool chessR[50];
 bool chessC[50];
 bool chessD1[99];
 bool chessD2[99];
-void solve(int x){
+void X(int x){
 	if(x == i){
 		m++;
 	    return;
@@ -30,17 +31,17 @@ void solve(int x){
 	        	if(now->next != nullptr){
 	        		now->prev->next = now->next;
 	        		now->next->prev = now->prev;
-	        		solve(x + 1);
+	        		X(x + 1);
 	        		now->next->prev = now;
 	        		now->prev->next = now;
 				}else{
 					now->prev->next = now->next;
-	        		solve(x + 1);
+	        		X(x + 1);
 	        		now->prev->next = now;
 				}
 			}else{
 				head = now->next;
-        		solve(x + 1);
+        		X(x + 1);
         		head = now;
 			}
 			chessD1[now_x + now->num] = false;
@@ -48,24 +49,24 @@ void solve(int x){
 	    }
 	}
 }
-int main(){
+void solve(tTestData* test_data){
 	C[0].next = &C[1];
 	for(i = 1; i < 19; i++){
 		C[i].next = &C[i + 1];
 		C[i].prev = &C[i - 1];
 	}
 	C[19].prev = &C[18];
-	ifstream in("input.txt", ios::in);
 	ofstream out("output.txt", ios::out);
-	in >> t;
-	while(t--){
-		in >> n >> m;
+	for(k = 0; k < test_data->t; k++){
+		n = test_data->testcase[k].n;
+		m = test_data->testcase[k].m;
 		for(i = 0; i < n; i++) chessR[i] = false;
 		for(i = 0; i < n; i++) chessC[i] = false;
 		for(i = 0; i < 2 * n - 1; i++) chessD1[i] = false;
 		for(i = 0; i < 2 * n - 1; i++) chessD2[i] = false;
 		for(i = 0; i < m; i++){
-			in >> x >> y;
+			x = test_data->testcase[k].preplace[i][0];
+			y = test_data->testcase[k].preplace[i][1];
 			chessR[x] = true;
 			chessC[y] = true;
 			chessD1[x + y] = true;
@@ -78,9 +79,8 @@ int main(){
 		i = n - m;
 		m = 0;
 		C[i - 1].next = nullptr;
-		solve(0);
+		X(0);
 		out << m << '\n';
 		C[i - 1].next = &C[i];
 	}
-	return 0;
 }
