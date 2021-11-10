f74a0f972ca50e287d1c5229c66fd3de481e4733
Pin-Shao Chen <hardymike6308@gmail.com>
2021-11-10 19:04:29 +0800

    HW4_1.050

--- HW4/HW4.cpp
+++ HW4/HW4.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#define test 10
 using namespace std;
 
 long long count1 = 0;
+long long count2 = 0;
 
 void merge1(vector<int>& initial, vector<int>& result, const int start, const int mid, const int end){
     int iR = end, iL = mid;
     for(int i = end; i >= start; i--){
         if(iR - mid - 1 < 0)    result[i] = initial[iL--];
         else if(iL - start < 0) result[i] = initial[iR--];
-        else if(initial[iR] >= initial[iL]){
+        else if(initial[iR] < initial[iL]){
             result[i] = initial[iR--];
         }else{
+        	count2 += iR - mid;
             result[i] = initial[iL--];
         }
         
@@ -22,8 +25,8 @@ void merge1(vector<int>& initial, vector<int>& result, const int start, const in
     for(int i = end; i >= start; i--){
         if(iR - mid - 1 < 0)    break;
         else if(iL - start < 0) break;
-        else if((long long)2 * initial[iR] > initial[iL]){
-            iR--;
+        else if((long long)initial[iL] + initial[iL] > initial[iR]){
+        	iR--;
         }else{
             count1 += iR - mid;
             iL--;
@@ -41,13 +44,35 @@ void mergePass(vector<int>& initial, vector<int>& result, const int size){
 }
 
 void merge_sort(vector<int>& arr){
-    vector<int> temp;
-    temp.resize(arr.size());
-    for(int size = 1; size < arr.size(); size *= 2){
+	vector<int> temp;
+	temp.resize(arr.size());
+    for(int size = test; size < arr.size(); size *= 2){
         mergePass(arr, temp, size);
         size *= 2;
         mergePass(temp, arr, size);
     }
+}
+vector<int> arr;
+vector<int> temp;
+void insert_sort(const int start, const int end){
+	//cout << end << '\n';
+	for(int i = start; i <= end; i++){
+		int j;
+		for(j = i-1; j >= start; j--){
+			if(arr[i] <= temp[j]){
+				break;
+			}
+		}
+		for(int k = i-1; k >= start; k--){
+			if((long long)temp[k] + temp[k] <= arr[i]){
+				count1++;
+			}
+		}
+		for(int k = i; k > j + 1; k--){
+			temp[k] = temp[k-1];
+		}
+		temp[j+1] = arr[i];
+	}
 }
 
 int main(){
@@ -60,17 +85,18 @@ int main(){
 		count1 = 0;
 		ans = 0;
 		in >> n;
-		vector<int> arr;
+		cout << n << '\n';
 		arr.resize(n);
 		for(int i = 0; i < n; i++){
 			in >> arr[i];
 		}
-		for(int i = 0; i < n / 2; i++){
-			int temp = arr[i];
-			arr[i] = arr[n-1-i];
-			arr[n-1-i] = temp;
+		temp.resize(n);
+		int i;
+		for(i = test; i < n; i += test){
+			insert_sort(i - test, i - 1);
 		}
-		merge_sort(arr);
+		insert_sort(i - test, n - 1);
+		merge_sort(temp);
 		out << count1 << '\n';
 	}
 	return 0;
--- HW4/HW4_shm_fast.cpp
+++ HW4/HW4_shm_fast.cpp
@@ -5,16 +5,36 @@
 using namespace std;
 
 int_fast64_t count1;
-int t, i, j;
-vector<int32_t> arr;
-vector<int32_t> temp;
+int n, t, i, j;
+vector<int32_t> arr(500000);
+vector<int32_t> temp(500000);
+
+void insert_sort(const int start, const int end){
+	for(int i = start; i <= end; i++){
+		int j;
+		for(j = i-1; j >= start; j--){
+			if(temp[i] <= arr[j]){
+				break;
+			}
+		}
+		for(int k = i-1; k >= start; k--){
+			if((long long)arr[k] + arr[k] <= temp[i]){
+				count1++;
+			}
+		}
+		for(int k = i; k > j + 1; k--){
+			arr[k] = arr[k-1];
+		}
+		arr[j+1] = temp[i];
+	}
+}
 
 void merge1(vector<int32_t>& initial, vector<int32_t>& result, const int start, const int mid, const int end){
     int iR = end, iL = mid;
     for(int i = end; i >= start; i--){
         if(iR - mid - 1 < 0)    result[i] = initial[iL--];
         else if(iL - start < 0) result[i] = initial[iR--];
-        else if(initial[iR] >= initial[iL]){
+        else if(initial[iR] < initial[iL]){
             result[i] = initial[iR--];
         }else{
             result[i] = initial[iL--];
@@ -25,8 +45,8 @@ void merge1(vector<int32_t>& initial, vector<int32_t>& result, const int start, 
     for(int i = end; i >= start; i--){
         if(iR - mid - 1 < 0)    break;
         else if(iL - start < 0) break;
-        else if((long long)initial[iR] + initial[iR] > initial[iL]){
-            iR--;
+        else if((long long)initial[iL] + initial[iL] > initial[iR]){
+        	iR--;
         }else{
             count1 += iR - mid;
             iL--;
@@ -34,33 +54,40 @@ void merge1(vector<int32_t>& initial, vector<int32_t>& result, const int start, 
     }
 }
 
-void mergePass(vector<int32_t>& initial, vector<int32_t>& result, const int size){
-    int i;
-    for(i = 0; i < (int) initial.size() - 2 * size + 1; i += 2 * size){
-        merge1(initial, result, i, i + size - 1, i + 2 * size - 1);
-    }
-    if(initial.size() - i + 1 > size)   merge1(initial, result, i, i + size - 1, initial.size() - 1);
-    else                            	merge1(initial, result, i, initial.size() - 1, initial.size() - 1);
-}
-
-void merge_sort(vector<int32_t>& arr){
-    temp.resize(arr.size());
-    for(int size = 1; size < arr.size(); size *= 2){
-        mergePass(arr, temp, size);
-        size *= 2;
-        mergePass(temp, arr, size);
-    }
+void merge_sort(){
+    for(int size = 10; size < n; size += size){
+        //mergePass(arr, temp, size);
+		int i;
+	    for(i = 0; i < n - 2 * size + 1; i += size + size){
+	        merge1(arr, temp, i, i + size - 1, i + 2 * size - 1);
+	    }
+	    if(n - i + 1 > size)   merge1(arr, temp, i, i + size - 1, n - 1);
+	    else                   merge1(arr, temp, i, n - 1, n - 1);
+        //
+		size += size;
+        //mergePass(temp, arr, size);
+	    for(i = 0; i < n - 2 * size + 1; i += 2 * size){
+	        merge1(temp, arr, i, i + size - 1, i + 2 * size - 1);
+	    }
+	    if(n - i + 1 > size)  merge1(temp, arr, i, i + size - 1, n - 1);
+	    else                  merge1(temp, arr, i, n - 1, n - 1);
+    	//
+	}
 }
 
 void solve(tTestData* test_data){
 	ofstream out("output.txt");
 	for(t = 0; t < test_data->testcase_num; t++){
 		count1 = 0;
-		arr.resize(test_data->seq_num[t]);
-		for(i = 0, j = test_data->seq_num[t] - 1; j >= 0; i++, j--){
-			arr[j] = test_data->seq[t][i];
+		n = test_data->seq_num[t];
+		for(i = 0; i < n; i++){
+			temp[i] = test_data->seq[t][i];
 		}
-		merge_sort(arr);
+		for(i = 10; i < n; i += 10){
+			insert_sort(i - 10, i - 1);
+		}
+		insert_sort(i - 10, n - 1);
+		merge_sort();
 		out << count1 << '\n';
 	}
 }
